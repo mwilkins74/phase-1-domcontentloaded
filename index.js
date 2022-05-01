013a5d3246bd9f95fc4f03b1329d58a4ef4e3c00
Mark <markwilkins86@gmail.com>
2022-05-01 16:26:51 -0400

    commit tree f98857a3a8a52ed04ab2fa4c3342edd15cd63086

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    editDOM();
+  });
+
+  function editDOM() {
+      document.getElementById('text');
+      text.innerHTML = 'This is really cool!'
+  }
